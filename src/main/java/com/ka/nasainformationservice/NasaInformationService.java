837029069c6ced38066fb677dd9f0277fc679372
Keith Aston <Keith.aston88@gmail.com>
2023-08-03 16:38:35 +0700

    Added Controller and added starter dependencies

---
+++ src/main/java/com/ka/nasainformationservice/NasaInformationService.java
@@ -0,0 +1,14 @@
+package com.ka.nasainformationservice;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaInformationService
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(NasaInformationService.class, args);
+    }
+}
