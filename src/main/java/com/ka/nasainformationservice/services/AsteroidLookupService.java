459578bf58bea8fd8f981b40ed1b2bbef6a22d88
Keith Aston <Keith.aston88@gmail.com>
2024-02-22 20:48:59 +0000

    Adding logic and tests for lookup service

--- src/main/java/com/ka/nasainformationservice/services/AsteroidLookupService.java
+++ src/main/java/com/ka/nasainformationservice/services/AsteroidLookupService.java
@@ -6,16 +6,12 @@ import com.ka.nasainformationservice.helpers.MainHelper;
 import com.ka.nasainformationservice.models.Asteroid;
 import com.ka.nasainformationservice.models.AsteroidLookupResponse;
 import com.ka.nasainformationservice.models.SearchDates;
+import com.ka.nasainformationservice.utils.CommonUtils;
 import lombok.AllArgsConstructor;
 import lombok.extern.apachecommons.CommonsLog;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
-
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @Service
 @AllArgsConstructor
@@ -23,10 +19,35 @@ import java.util.Date;
 public class AsteroidLookupService {
 
     private final NasaIntegrator nasaIntegrator;
+    private final CommonUtils commonUtils;
 
-    public AsteroidLookupResponse getAsteroidByDate(SearchDates searchDates) throws Exception {
+    public Asteroid getAsteroidByID(String asteroidID){
         String response;
-        response = nasaIntegrator.getAsteroidByDates(searchDates);
+        try {
+            response = nasaIntegrator.getAsteroidById(asteroidID);
+        } catch (APIKeyInvalidException e) {
+            log.error(MainHelper.API_KEY_INVALID_ASTEROID_ID);
+            return null;
+        } catch (Exception e) {
+            log.error(MainHelper.ASTEROID_BY_ID_GENERIC_ERROR + e.getMessage());
+            return null;
+        }
+
+        return commonUtils.populateAsteroidLookupInfo(new JSONObject(response));
+    }
+
+    public AsteroidLookupResponse getAsteroidByDate(SearchDates searchDates) {
+        String response;
+        try {
+            response = nasaIntegrator.getAsteroidByDates(searchDates);
+        } catch (APIKeyInvalidException e) {
+            log.error(MainHelper.API_KEY_INVALID_ASTEROID_DATES);
+            return null;
+        } catch (Exception e) {
+            log.error(MainHelper.ASTEROID_BY_DATE_GENERIC_ERROR + e.getMessage());
+            return null;
+        }
+
         return parseAsteroidData(response, searchDates);
     }
 
@@ -56,77 +77,12 @@ public class AsteroidLookupService {
 
         for (int i = 0; i < asteroidCount && i < 5; i++) {
             JSONObject asteroidInfo = (JSONObject) objectsArray.get(i);
-            Asteroid asteroid = populateAsteroidLookupInfo(asteroidInfo);
+            Asteroid asteroid = commonUtils.populateAsteroidLookupInfo(asteroidInfo);
             asteroids[i] = asteroid;
         }
         return asteroids;
     }
 
-    public Asteroid getAsteroidByID(String asteroidID){
-        String response;
-        try {
-            response = nasaIntegrator.getAsteroidById(asteroidID);
-        } catch (APIKeyInvalidException e) {
-            log.error(e.getMessage());
-            return null;
-        }
-        return populateAsteroidLookupInfo(new JSONObject(response));
-    }
 
-    private Asteroid populateAsteroidLookupInfo(JSONObject json) {
-        Asteroid asteroid = new Asteroid();
-        asteroid.setId(Integer.parseInt(json.getString(MainHelper.ID_KEY)));
-        asteroid.setName(json.getString(MainHelper.NAME_KEY));
-        asteroid.setIsPotentiallyHazardous(json.getBoolean(MainHelper.IS_POTENTIALLY_DANGEROUS_KEY));
 
-        asteroid = extractAsteroidMetrics(asteroid, json);
-        return asteroid;
-    }
-
-    private Asteroid extractAsteroidMetrics(Asteroid asteroid, JSONObject response){
-
-        JSONObject estimatedDiameters = response.getJSONObject(MainHelper.ESTIMATED_DIAMETER_KEY);
-        JSONObject kilometers = estimatedDiameters.getJSONObject(MainHelper.KILOMETERS_KEY);
-
-        asteroid.setEstimatedDiameterInKMs_Min(kilometers.getDouble(MainHelper.ESTIMATED_DIAMETER_MIN_KEY));
-        asteroid.setEstimatedDiameterInKMs_Max(kilometers.getDouble(MainHelper.ESTIMATED_DIAMETER_MAX_KEY));
-
-        return populateCloseApproachData(asteroid, response);
-    }
-
-    private Asteroid populateCloseApproachData(Asteroid asteroid, JSONObject response){
-        JSONArray closeApproachDataArray = response.getJSONArray(MainHelper.CLOSE_APPROACH_DATA_KEY);
-        DateFormat formatter = new SimpleDateFormat(MainHelper.DATE_FORMAT);
-        Date todayDate = new Date();
-        JSONObject approachInfo;
-        Date approachDate;
-        log.info(MainHelper.PARSING_CLOSE_APPROACH_DATA);
-
-        try {
-            for (int i = 0; i < closeApproachDataArray.length(); i++) {
-                approachInfo = closeApproachDataArray.getJSONObject(i);
-                approachDate = (Date) formatter.parse(approachInfo.getString(MainHelper.CLOSE_APPROACH_DATE_KEY));
-
-                if (approachDate.compareTo(todayDate) >= 0 || closeApproachDataArray.length() == 1) {
-                    asteroid.setCloseApproachDate(formatter.format(approachDate));
-
-                    JSONObject relativeVelocity = approachInfo.getJSONObject(MainHelper.RELATIVE_VELOCITY_KEY);
-                    asteroid.setRelativeVelocityKmph(Double.parseDouble(relativeVelocity.getString(MainHelper.KMPH_KEY)));
-
-                    JSONObject missDistance = approachInfo.getJSONObject(MainHelper.MISS_DIST_KEY);
-                    asteroid.setMissDistanceKm(Double.parseDouble(missDistance.getString(MainHelper.KILOMETERS_KEY)));
-
-                    log.info(MainHelper.PARSING_CLOSE_APPROACH_DATA_SUCCESS);
-                    return asteroid;
-                }
-            }
-            return asteroid;
-        } catch (ParseException parseException) {
-            log.error(MainHelper.PARSING_CLOSE_APPROACH_DATA_EXCEPTION + parseException);
-            return asteroid;
-        } catch (Exception e) {
-            log.error(MainHelper.PARSING_CLOSE_APPROACH_DATA_UNKNOWN_EXCEPTION + e);
-            return asteroid;
-        }
-    }
 }
---
+++ src/test/java/com/ka/nasainformationservice/services/AsteroidLookupServiceTest.java
@@ -0,0 +1,98 @@
+package com.ka.nasainformationservice.services;
+
+import com.ka.nasainformationservice.Exceptions.APIKeyInvalidException;
+import com.ka.nasainformationservice.Integrators.NasaIntegrator;
+import com.ka.nasainformationservice.models.Asteroid;
+import com.ka.nasainformationservice.models.AsteroidLookupResponse;
+import com.ka.nasainformationservice.models.SearchDates;
+import com.ka.nasainformationservice.utils.CommonUtils;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class AsteroidLookupServiceTest {
+
+    @Mock
+    private NasaIntegrator nasaIntegrator;
+    @Mock
+    private CommonUtils commonUtils;
+
+    private AsteroidLookupService asteroidLookupService;
+    private static final String AsteroidsResponse_FILENAME = "AsteroidsResponse.json";
+    private String asteroidResponse;
+
+    @Before
+    public void setup() throws IOException {
+        MockitoAnnotations.initMocks(this);
+        asteroidLookupService = new AsteroidLookupService(nasaIntegrator,commonUtils);
+        ResourceLoader resourceLoader = new DefaultResourceLoader();
+
+        Resource asteroidResource = resourceLoader.getResource("classpath:" + AsteroidsResponse_FILENAME);
+        Reader asteroidReader = new InputStreamReader(asteroidResource.getInputStream(), UTF_8);
+        asteroidResponse = FileCopyUtils.copyToString(asteroidReader);
+    }
+
+    @Test
+    public void test_getAsteroidById() throws Exception {
+        when(nasaIntegrator.getAsteroidById("123")).thenReturn(getSampleJson());
+        when(commonUtils.populateAsteroidLookupInfo(any(JSONObject.class))).thenReturn(getAsteroid());
+        Asteroid asteroid = asteroidLookupService.getAsteroidByID("123");
+        assertEquals(asteroid.getId(),123);
+        assertEquals(asteroid.getIsPotentiallyHazardous(), true);
+    }
+
+    @Test
+    public void test_getAsteroidById_InvalidAPIKey() throws Exception {
+        when(nasaIntegrator.getAsteroidById("123")).thenThrow(new APIKeyInvalidException("API Key Invalid"));
+        Asteroid asteroid = asteroidLookupService.getAsteroidByID("123");
+        assertNull(asteroid);
+    }
+
+    @Test
+    public void test_getAsteroidByDates() throws Exception {
+        when(nasaIntegrator.getAsteroidByDates(getSearchDates())).thenReturn(asteroidResponse);
+        AsteroidLookupResponse asteroidLookupResponse = asteroidLookupService.getAsteroidByDate(getSearchDates());
+        Asteroid[] asteroids = asteroidLookupResponse.getAsteroids();
+
+        assertEquals(asteroidLookupResponse.getNumOfAsteroidsFound(),5);
+        assertEquals(asteroidLookupResponse.getStart_date(), "2015-09-21");
+        assertEquals(asteroidLookupResponse.getEnd_date(), "2015-09-22");
+        assertEquals(asteroids.length, 5);
+    }
+
+    private Asteroid getAsteroid(){
+        Asteroid asteroid = new Asteroid();
+        asteroid.setName("test");
+        asteroid.setMissDistanceKm(455);
+        asteroid.setRelativeVelocityKmph(54);
+        asteroid.setIsPotentiallyHazardous(true);
+        asteroid.setId(123);
+        return asteroid;
+    }
+
+    private SearchDates getSearchDates(){
+        return new SearchDates("2015-09-21", "2015-09-22");
+    }
+
+    private String getSampleJson(){
+        return "{\"key\": \"value\"}";
+    }
+
+}
